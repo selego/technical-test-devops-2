18199a4caeae3e5cca13961548b781857f575434
Nazakkour <azakkour.nabil@gmail.com>
2024-09-26 17:58:40 +0200

    Uncomment the error in the integration test

--- test/api.test.js
+++ test/api.test.js
@@ -16,7 +16,7 @@ describe('API Routes', () => {
         const response = await request(app).get('/health');
         expect(response.status).toBe(200);
     });
-/*
+
     it('should return a secret value for the /secret route with valid API key', async () => {
         const response = await request(app)
             .get('/secret')
@@ -25,7 +25,7 @@ describe('API Routes', () => {
         expect(response.status).toBe(200);
         expect(response.text).toBe(`Secret value is: ${process.env.SECRET_ENV}`);
     });
-*/
+
     it('should return 403 for the /secret route with invalid API key', async () => {
         const response = await request(app)
             .get('/secret')
@@ -33,12 +33,12 @@ describe('API Routes', () => {
 
         expect(response.status).toBe(403); // Forbidden
     });
-/*
+
     it('should return 200 for the /bug route', async () => {
         const response = await request(app).get('/bug');
         expect(response.status).toBe(200);
     });
-*/
+
     it('should handle errors on /error route', async () => {
         const response = await request(app).get('/error');
         expect(response.status).toBe(500); // Internal Server Error
